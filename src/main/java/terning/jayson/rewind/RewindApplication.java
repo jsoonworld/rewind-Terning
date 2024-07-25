3ed002f4836030321eb9f3bddc7bcc6cbdcfdeee
권장순 <jsoonworld@gmail.com>
2024-07-25 16:39:10 +0900

    feat: init

---
+++ src/main/java/terning/jayson/rewind/RewindApplication.java
@@ -0,0 +1,13 @@
+package terning.jayson.rewind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/terning/jayson/rewind/RewindApplicationTests.java
@@ -0,0 +1,13 @@
+package terning.jayson.rewind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
